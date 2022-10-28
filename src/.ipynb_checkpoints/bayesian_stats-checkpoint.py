fce96589b42a9ece59d7ab74cc1a45599f222992
hyosubkim <32351455+hyosubkim@users.noreply.github.com>
2022-10-28 17:31:42 -0400

    adding docstrings and changing BEST priors

--- src/.ipynb_checkpoints/bayesian_stats-checkpoint.py
+++ src/.ipynb_checkpoints/bayesian_stats-checkpoint.py
@@ -1,12 +1,20 @@
 import arviz as az
 import numpy as np
 import pandas as pd
-import pymc3 as pm
+import pymc as pm
 import seaborn as sns
 
 
 def standardize(x):
+    """Standardizes the input variable.
     
+    Arguments:
+        x (ndarray/Series): the variable to standardize 
+    
+    Returns:
+        ndarray/Series: the standardized version of the input variable x
+    """
+        
     return (x - x.mean()) / x.std()
 
 
@@ -38,7 +46,18 @@ def compare_one_group(y, group=None, n_samples=1000):
     return idata
 
 
-def compare_two_groups(y, group, sigma_low, sigma_high, n_draws=1000):
+def BEST(y, group, n_draws=1000):
+    """Implementation of John Kruschke's BEST test.
+    
+    Compares outcomes from two groups and estimates parameters.
+    
+    Arguments:
+        y (ndarray/Series): The metric outcome variable.
+        group: The grouping variable providing that indexes into y.
+    
+    Returns: 
+        PyMC Model and InferenceData objects.
+    """
     
     # Convert grouping variable to categorical dtype if it is not already
     if pd.api.types.is_categorical_dtype(group):
@@ -54,36 +73,38 @@ def compare_two_groups(y, group, sigma_low, sigma_high, n_draws=1000):
     y_group1 = y[group==level[0]]
     y_group2 = y[group==level[1]]
     
-    # Arbitrarily set hyperparameters to the pooled empirical mean of data and twice pooled empirical SD, 
-    # which applies very diffuse info to these quantities and does not favor one or the other a priori
-    mu_m = y.mean()
-    mu_s = y.std() * 2
-
+    # Calculate pooled empirical mean and SD of data to scale hyperparameters
+    mu_y = y.mean()
+    sigma_y = y.std()
+    
+    # Arbitrarily set hyperparameters to the pooled empirical mean of data and 
+    # twice pooled empirical SD, which applies very diffuse info to these 
+    # quantities and does not favor one or the other a priori
+    mu_m = mu_y
+    mu_s = sigma_y * 2
+                                                                     
     with pm.Model() as model:
-        # Set priors
+        # Define priors
         group1_mean = pm.Normal("group1_mean", mu=mu_m, sigma=mu_s)
         group2_mean = pm.Normal("group2_mean", mu=mu_m, sigma=mu_s)
-        group1_std = pm.Uniform("group1_std", lower=sigma_low, upper=sigma_high)
-        group2_std = pm.Uniform("group2_std", lower=sigma_low, upper=sigma_high)
+        group1_std = pm.Uniform("group1_std", lower=sigma_y / 10, upper=sigma_y * 10)
+        group2_std = pm.Uniform("group2_std", lower=sigma_y / 10, upper=sigma_y * 10)
         
-        # See Kruschke Ch 16.2.1 for in-depth rationale for prior on nu. Briefly, for values of nu greater than
-        # or equal to 30, the distribution converges to a standard normal. Since we want to allow for smaller and
-        # larger values of nu, we are setting the prior to be an exponential with a mean of 30 (i.e., there is
-        # substantial probability for smaller values of nu). The single parameter for an exponential is the reciprocal
-        # of the mean. The addition of 1 is to shift the distribution so that possible possible values of nu are 1 to 
-        # infinity (and the mean of the distribution is 30).
+        # See Kruschke Ch 16.2.1 for in-depth rationale for prior on nu. The addition of 1 is to shift the
+        # distribution so that the range of possible values of nu are 1 to infinity (with mean of 30).
         nu_minus_one = pm.Exponential("nu_minus_one", 1 / 29)
         nu = pm.Deterministic("nu", nu_minus_one + 1)
+        nu_log10 = pm.Deterministic("nu_log10", np.log10(nu))
         
         # Need to convert SD to precision for model
         lambda_1 = group1_std**-2
         lambda_2 = group2_std**-2
         
-        # Likelihood
-        like1 = pm.StudentT("group1", nu=nu, mu=group1_mean, lam=lambda_1, observed=y_group1)
-        like2 = pm.StudentT("group2", nu=nu, mu=group2_mean, lam=lambda_2, observed=y_group2)
+        # Define likelihood
+        likelihood1 = pm.StudentT("group1", nu=nu, mu=group1_mean, lam=lambda_1, observed=y_group1)
+        likelihood2 = pm.StudentT("group2", nu=nu, mu=group2_mean, lam=lambda_2, observed=y_group2)
         
-        # Contrasts
+        # Contrasts of interest
         diff_of_means = pm.Deterministic("difference of means", group1_mean - group2_mean)
         diff_of_stds = pm.Deterministic("difference of stds", group1_std - group2_std)
         effect_size = pm.Deterministic(
@@ -91,14 +112,15 @@ def compare_two_groups(y, group, sigma_low, sigma_high, n_draws=1000):
         )
         
         # Sample from posterior
-        idata = pm.sample(draws=n_draws, return_inferencedata=True)
+        idata = pm.sample(draws=n_draws)
         
     return model, idata
 
 
-def robust_linear_regression():
+def robust_linear_regression(n_draws=1000):
     
     with pm.Model() as model:
+        # Define priors
         beta0 = pm.Normal('beta0', mu=0, tau=1/10**2)
         beta1 = pm.Normal('beta1', mu=0, tau=1/10**2)
         mu = beta0 + beta1*x
@@ -108,7 +130,7 @@ def robust_linear_regression():
 
         likelihood = pm.StudentT('likelihood', nu, mu=mu, sd=sigma, observed=y)
 
-        idata = pm.sample(return_inferencedata=True)
+        idata = pm.sample(draws=n_draws)
     
     return idata
 
