da78e38c929310a047113c078032d85b695678b5
hyosubkim <32351455+hyosubkim@users.noreply.github.com>
2022-11-08 16:21:41 -0500

    contrasts with label-based indexing

--- src/.ipynb_checkpoints/bayesian_stats-checkpoint.py
+++ src/.ipynb_checkpoints/bayesian_stats-checkpoint.py
@@ -438,11 +438,11 @@ def metric_outcome_one_nominal_predictor(x, y, mu_y, sigma_y, n_draws=1000):
     
     a_shape, a_rate = gamma_shape_rate_from_mode_sd(sigma_y / 2, 2 * sigma_y)
     
-    with pm.Model() as model:
+    with pm.Model(coords={"groups": levels}) as model:
         # 'a' indicates coefficients not yet obeying sum-to-zero contraint
         sigma_a = pm.Gamma('sigma_a', alpha=a_shape, beta=a_rate)
         a0 = pm.Normal('a0', mu=mu_y, sigma=sigma_y * 5)
-        a = pm.Normal('a', 0.0, sigma=sigma_a, shape=n_levels)
+        a = pm.Normal('a', 0.0, sigma=sigma_a, dims="groups")
 
         sigma_y = pm.Uniform('sigma_y', sigma_y / 100, sigma_y * 10)
         likelihood = pm.Normal('likelihood', a0 + a[x_vals], sigma=sigma_y, observed=y)
