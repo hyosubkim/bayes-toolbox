22f20a0cef35613dc217437869502bce974c3f72
Hyosub Kim <hyosubkim1@gmail.com>
2023-04-20 11:04:05 -0400

    minor clean-up

--- bayes_toolbox/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("bayes_toolbox")
-except PackageNotFoundError:
-    # If the package is not installed, don't add __version__
-    pass
