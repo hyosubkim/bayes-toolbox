1e4257146639701c9a65b129196cfac16a4d4bb5
Hyosub Kim <hyosubkim1@gmail.com>
2023-11-29 11:48:54 -0800

    Add dynamic versioning in .toml file

--- bayes_toolbox/__init__.py
+++ bayes_toolbox/__init__.py
@@ -1,5 +1,14 @@
 # file: __init__.py
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("bayes_toolbox")
+except PackageNotFoundError:
+    # If the package is not installed, don't add __version__
+    pass
+
+
 try:
     from importlib.metadata import version, PackageNotFoundError
 except ImportError:
